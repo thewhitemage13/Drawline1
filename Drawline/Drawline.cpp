beb1b0a12f76baa5835a5d352b1b93cc42ca0b72
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-09-20 22:28:02 +0200

    Add files via upload

---
+++ Drawline/Drawline.cpp
@@ -0,0 +1,27 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+void DrawLine(int length, char symbol, int color, bool isHorizontal)
+{
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(h, color);
+    if (isHorizontal)
+    {
+        for (int i = 0; i < length; ++i)
+         cout << symbol;
+    }
+    else
+    {
+        for (int i = 0; i < length; ++i)
+        {
+            cout << symbol << "\n";
+        }
+    }
+    SetConsoleTextAttribute(h, 7);
+}
+int main()
+{
+    DrawLine(20, '@', 12, true);
+    DrawLine(12, '@', 14, false);
+    return 0;
+} 
